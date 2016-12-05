e36331cde5205e3440a09b83d119e16ed2759436
Rocky <rocky.amdad@gmail.com>
2016-12-05 17:44:33 +0600

    ninja tutirail 10 to 15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import {HomeComponent} from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  directives: [HomeComponent]
 })
 export class AppComponent {
-  title = 'app works!';
+
+  title = 'Angular 2 test Apllication';
+  name = 'Rocky From RBS';
+
+  animals ={
+    'name':'Cat',
+    'type': 'Persian'
+  };
+  fire(e){
+    alert('i am listening');
+  }
+
 }
